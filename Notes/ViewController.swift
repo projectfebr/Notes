9455c3e74ce1f85d0b369f440b1cbd1394943820
Aleksandr Bolotov <Alexander.Bolotov@onlyoffice.com>
2022-06-14 23:08:12 +0300

    Initial Commit

---
+++ Notes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Aleksandr Bolotov on 14.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
