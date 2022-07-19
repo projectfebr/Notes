aa2ccc6825afc21853a776fc752cf9cf02ad2195
Aleksandr Bolotov <Alexander.Bolotov@onlyoffice.com>
2022-07-19 17:53:53 +0300

    add CustomProgoreeView

---
+++ Notes/Presentation/Views/CustomProgressView.swift
@@ -0,0 +1,26 @@
+//
+//  CustomProgressView.swift
+//  Notes
+//
+//  Created by Aleksandr Bolotov on 19.07.2022.
+//
+
+import UIKit
+
+@IBDesignable
+class CustomProgressView: UIProgressView {
+
+    @IBInspectable
+    public var cornerRadius: CGFloat = 2 {
+        didSet {
+            layer.cornerRadius = self.cornerRadius
+        }
+    }
+
+    @IBInspectable
+    public var heightScale: CGFloat = 1 {
+        didSet {
+            transform = transform.scaledBy(x: 1, y: heightScale)
+        }
+    }
+}
